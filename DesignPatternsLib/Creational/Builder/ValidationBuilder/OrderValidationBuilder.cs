1effed19425b420d9bfe81b466742042cee69c9f
shelby-desk <shelbyisa9139@gmail.com>
2021-02-26 06:56:23 -0800

    Finish order validation

--- DesignPatternsLib/Creational/Builder/ValidationBuilder/OrderValidationBuilder.cs
+++ DesignPatternsLib/Creational/Builder/ValidationBuilder/OrderValidationBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DesignPatternsLib.Creational.Builder.ValidationBuilder
@@ -14,6 +15,9 @@ namespace DesignPatternsLib.Creational.Builder.ValidationBuilder
         private Order order;
         private bool success;
         private string errorMessages;
+        private Regex emailRegex = new Regex("", RegexOptions.IgnoreCase);
+        private Regex phoneRegex = new Regex("");
+        private Regex nonNumeric = new Regex(@"[^\d]");
 
         public void SetOrder(Order order)
         {
@@ -22,17 +26,57 @@ namespace DesignPatternsLib.Creational.Builder.ValidationBuilder
 
         public void ValidatePayment()
         {
-            throw new NotImplementedException();
+            // In a real-life situation, would need to validate
+            // the payment credit card and billing address,
+            // probably with some external api.
+            return;
         }
 
         public void ValidatePet()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(order.Pet.Name))
+                throw new Exception("Pet name cannot be empty.");
+            
+            if (string.IsNullOrWhiteSpace(order.Pet.Breed))
+                throw new Exception("Pet breed cannot be empty.");
+
+            switch(order.Pet.Sex.ToLower())
+            {
+                case "m":
+                case "male":
+                    // set some enum to Male
+                    break;
+                case "f":
+                case "female":
+                    // set some enum to Female
+                    break;
+                default:
+                    throw new Exception($"Pet sex was not in a correct format. Pet.Sex: {order.Pet.Sex}");
+            }
         }
 
         public void ValidateUser()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(order.User.Name))
+                throw new Exception("User name cannot be empty.");
+
+            if (!emailRegex.IsMatch(order.User.Email))
+                throw new Exception($"User email was not in a correct format. User.Email: {order.User.Email}");
+
+            if (!phoneRegex.IsMatch(order.User.Phone))
+                throw new Exception($"User phone was not in a correct format. User.Phone: {order.User.Phone}");
+            else
+            {
+                string digitsOnly = nonNumeric.Replace(order.User.Phone, "");
+                if (digitsOnly.Count() == 7)
+                    order.User.Phone = $"{digitsOnly.Remove(0,3)}-{digitsOnly.Remove(0,4)}";
+                else if (digitsOnly.Count() == 10)
+                    order.User.Phone = $"({digitsOnly.Remove(0,3)}) {digitsOnly.Remove(0,3)}-{digitsOnly.Remove(0,4)}";
+                else if (digitsOnly.Count() == 11 && digitsOnly.First() == 1)
+                    order.User.Phone = $"{digitsOnly.Remove(0,1)}-{digitsOnly.Remove(0,3)}-{digitsOnly.Remove(0,3)}-{digitsOnly.Remove(0,4)}";
+                else
+                    throw new Exception($"User phone was not in a correct format. User.Phone: {order.User.Phone}");
+            }
         }
 
         public void ResetBuilder()
